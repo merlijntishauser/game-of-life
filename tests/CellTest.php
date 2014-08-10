5933dccb1b23ec1a7ee4650d4e6fe7b1025e7341
Danny van Dam <d.van.dam@vnumedia.nl>
2014-08-10 21:12:20 +0200

    Cells die when having more than 3 living neighbours

--- tests/CellTest.php
+++ tests/CellTest.php
@@ -5,9 +5,18 @@ class CellTest extends \PHPUnit_Framework_TestCase {
     /**
      * @test
      */
-    public function CellsDieWhenHavingLessThanTwoNeighbours()
+    public function cellsDieWhenHavingLessThanTwoNeighbours()
     {
         $cell = new Cell(true, 1);
+        $this->assertFalse($cell->willLive());
+    }
+
+    /**
+     * @test
+     */
+    public function cellsDieWhenHavingMoreThanThreeNeighbours()
+    {
+        $cell = new Cell(true, 4);
         $this->assertFalse($cell->willLive());
     }
 }
