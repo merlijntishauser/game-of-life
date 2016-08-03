f673ff667228de95ade8e4b7fbf1514d806f85b1
Merlijn Tishauser <merlijn@gargleblaster.org>
2016-08-03 10:01:51 +0200

    added 2 tests to make sure the grid->equals returns true when the grid is really equal

--- tests/unit/GameTest.php
+++ tests/unit/GameTest.php
@@ -23,4 +23,64 @@ class GameTest extends \PHPUnit_Framework_TestCase
         $resultGrid = $game->createNextGrid($originalGrid);
         $this->assertTrue($expectedGrid->equals($resultGrid));
     }
+
+    /**
+     * @test
+     */
+    public function equalsReturnsFalseWhenWidthIsChanged()
+    {
+        $originalGrid = new Grid(3, 3);
+        $originalGrid->setAlive(0, 1);
+        $originalGrid->setAlive(1, 1);
+        $originalGrid->setAlive(2, 1);
+
+        $expectedGrid = new Grid(4, 3);
+        $expectedGrid->setAlive(1, 0);
+        $expectedGrid->setAlive(1, 1);
+        $expectedGrid->setAlive(1, 2);
+
+        $game = new Game();
+        $resultGrid = $game->createNextGrid($originalGrid);
+        $this->assertFalse($expectedGrid->equals($resultGrid));
+    }
+
+    /**
+     * @test
+     */
+    public function equalsReturnsFalseWhenHeigthIsChanged()
+    {
+        $originalGrid = new Grid(3, 3);
+        $originalGrid->setAlive(0, 1);
+        $originalGrid->setAlive(1, 1);
+        $originalGrid->setAlive(2, 1);
+
+        $expectedGrid = new Grid(3, 4);
+        $expectedGrid->setAlive(1, 0);
+        $expectedGrid->setAlive(1, 1);
+        $expectedGrid->setAlive(1, 2);
+
+        $game = new Game();
+        $resultGrid = $game->createNextGrid($originalGrid);
+        $this->assertFalse($expectedGrid->equals($resultGrid));
+    }
+
+    /**
+     * @test
+     */
+    public function equalsReturnsFalseWhenCellisChanged()
+    {
+        $originalGrid = new Grid(3, 3);
+        $originalGrid->setAlive(0, 1);
+        $originalGrid->setAlive(1, 1);
+        $originalGrid->setAlive(2, 1);
+
+        $expectedGrid = new Grid(3, 3);
+        $expectedGrid->setAlive(1, 0);
+        $expectedGrid->setAlive(1, 1);
+        $expectedGrid->setAlive(2, 2);
+
+        $game = new Game();
+        $resultGrid = $game->createNextGrid($originalGrid);
+        $this->assertFalse($expectedGrid->equals($resultGrid));
+    }
 }
